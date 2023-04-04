c8f944d13442100e7971f2722f8688f127fad234
Antoine07 <antoine.lucsko@gmail.com>
2023-04-04 14:24:01 +0200

    tests cart storage

---
+++ TESTS/Cart/src/Product.php
@@ -0,0 +1,62 @@
+<?php
+
+class Product
+{
+
+    private string $name;
+    private float $price;
+
+    public function __construct(string $name, float $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * Get the value of name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of price
+     *
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set the value of price
+     *
+     * @param float $price
+     *
+     * @return self
+     */
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
